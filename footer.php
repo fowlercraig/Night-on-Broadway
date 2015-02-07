00e8ff9af40016e0a309e827c55a9f453a769e8a
Craig Fowler <craig@wearegiants.com>
2015-02-06 18:21:40 -0800

    Junk.

--- footer.php
+++ footer.php
@@ -25,6 +25,16 @@ twttr.conversion.trackPid('l5dta');
 </script>
 <noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev=6024076552740&amp;amp;cd[value]=0.00&amp;amp;cd[currency]=USD&amp;amp;noscript=1" /></noscript></p>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-35986901-14', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </body>
 </html>
