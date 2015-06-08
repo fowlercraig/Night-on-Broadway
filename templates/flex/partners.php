8291b2ce5b773ff9260c7908fa3fac23cf09b8a4
Craig Fowler <craig@wearegiants.com>
2015-06-08 10:29:31 -0400

    updated image description

--- templates/flex/partners.php
+++ templates/flex/partners.php
@@ -1,27 +1,15 @@
 <?php $images = get_sub_field('partners'); if( $images ): ?>
 
 <div class="row" id="partners">
-<<<<<<< HEAD
-
-<?php foreach( $images as $image ): ?>
-
-  <a href="#" class="partner <?php echo $image['caption']; ?> contained">
-    <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive" />
-  </a>
-
-<?php endforeach; ?>
-
-=======
   <div class="desktop-10 tablet-6 mobile-3 centered">
     <div class="row">
       <?php foreach( $images as $image ): ?>
-      <a href="<?php echo $image['url']; ?>" class="partner <?php echo $image['caption']; ?> contained">
+      <a href="<?php echo $image['description']; ?>" class="partner <?php echo $image['caption']; ?> contained">
       <img src="<?php echo $image['sizes']['large']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive" />
       </a>
       <?php endforeach; ?>
     </div>
   </div>
->>>>>>> origin/master
   <div class="desktop-12 tablet-6 mobile-3"><?php the_sub_field('simple_partners');?>
 </div>
 <?php endif; ?>
