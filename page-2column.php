9e5551d1af25366f213f876681991dad0f04f60c
Craig Fowler <craig@wearegiants.com>
2015-08-19 07:59:10 -0700

    added gallery

--- page-2column.php
+++ page-2column.php
@@ -3,7 +3,10 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <main class="page row">
-  <div class="page-content desktop-7 tablet-4 mobile-3"><?php the_post(); the_content(); ?></div>
+  <div class="page-content desktop-7 tablet-4 mobile-3">
+  	<?php the_post(); the_content(); ?>
+  	<?php include locate_template('flex/gallery.php'); ?>
+  </div>
   <div class="sidebar desktop-4 tablet-2 mobile-3 right"><?php the_field('sidebar_content'); ?></div>
 </main>
 
