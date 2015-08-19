85935f9746bc0bd1dacfbfef056137299cf0128a
Craig Fowler <craig@wearegiants.com>
2015-08-19 08:14:42 -0700

    gallery updates

--- page-2column.php
+++ page-2column.php
@@ -4,8 +4,9 @@
 
 <main class="page row">
   <div class="page-content desktop-7 tablet-4 mobile-3">
+  	<?php if(get_field('gallery_location') == "top"): echo '<hr class="invisible">'; include locate_template('templates/flex/gallery.php'); endif; ?>
   	<?php the_post(); the_content(); ?>
-  	<?php include locate_template('flex/gallery.php'); ?>
+  	<?php if(get_field('gallery_location') == "bottom"): include locate_template('templates/flex/gallery.php'); endif; ?>
   </div>
   <div class="sidebar desktop-4 tablet-2 mobile-3 right"><?php the_field('sidebar_content'); ?></div>
 </main>
